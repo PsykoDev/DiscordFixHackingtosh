439ec8479cdcd64f6be0f6a9d46d7bee399dae9b
PsykoDev <45910905+PsykoDev@users.noreply.github.com>
2022-06-11 17:06:36 +0200

    Add files via upload

---
+++ DiscordFixHackingtoshCPP/main.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//  DiscordFixHackingtoshCPP
+//
+//  Created by psyko on 11/06/2022.
+//
+
+#include <iostream>
+#include <stdlib.h>
+
+void Cmd(void);
+void Plist(void);
+
+int main(int argc, const char * argv[]) {
+    Cmd();
+    printf("Done!\n");
+    return 0;
+}
+
+void Cmd(){
+    system("killall Discord");
+    printf("Editing of Discord to discord_ / Debug CPU 5\n");
+    system("cd /Applications/Discord.app/Contents/MacOS && echo 'MKL_DEBUG_CPU_TYPE=5 ./Discord' > discord_");
+    printf(" Discord");
+    
+    Plist();
+}
+
+void Plist(){
+    system("/usr/libexec/PlistBuddy -c \"set :CFBundleExecutable discord_\" \"/Applications/Discord.app/Contents/Info.plist\"");
+    system("open /Applications/ -a Discord.app");
+}
